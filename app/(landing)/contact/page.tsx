a24d54e220987ada8521a406eec6d7c7a4be983a
Reghardt Pienaar <reghardt7@gmail.com>
2024-02-23 15:13:23 +0200

    .

--- app/(landing)/contact/page.tsx
+++ app/(landing)/contact/page.tsx
@@ -8,7 +8,6 @@ import React from "react";
 const ContactPage = () => {
   return (
     <div className="w-screen h-[100vh] lg:max-h-screen flex flex-col items-center overflow-hidden">
-      {/* <h2 className='booking_title text-center mb-14 flex flex-col items-center justify-center'>Contact</h2> */}
       <ViewMotionWrapper className="mt-8" duration={1}>
         <PageHeading />
       </ViewMotionWrapper>
