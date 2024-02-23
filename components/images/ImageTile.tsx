a04b2c3407dc8ec1c3e00163d3ba1c5e2a5e379a
Reghardt Pienaar <reghardt7@gmail.com>
2024-02-23 11:15:00 +0200

    .

--- components/images/ImageTile.tsx
+++ components/images/ImageTile.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-const ImageTile = ({ image }) => {
+const ImageTile = ({ image } : { image: any }) => {
   return (
     <div className='min-h-full min-w-full relative'>
       <Image src = {image} fill alt = 'an image' />
