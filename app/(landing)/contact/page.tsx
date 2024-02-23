9097a2d97a10b9ba4350c2a748d202c63e73937c
Reghardt Pienaar <reghardt7@gmail.com>
2024-02-23 14:41:30 +0200

    privacy policy

--- app/(landing)/contact/page.tsx
+++ app/(landing)/contact/page.tsx
@@ -1,17 +1,23 @@
-import Contact from '@/components/booking/Contact'
-import PageHeading from '@/components/headings/PageHeading'
-import React from 'react'
+"use client";
+
+import Contact from "@/components/booking/Contact";
+import PageHeading from "@/components/headings/PageHeading";
+import ViewMotionWrapper from "@/components/layout/Motion/ViewMotionWrapper";
+import React from "react";
 
 const ContactPage = () => {
   return (
-    <div className='w-screen h-[100vh] lg:max-h-screen flex flex-col items-center justify-center overflow-hidden'>
+    <div className="w-screen h-[100vh] lg:max-h-screen flex flex-col items-center overflow-hidden">
       {/* <h2 className='booking_title text-center mb-14 flex flex-col items-center justify-center'>Contact</h2> */}
-      <div className='mt-8'>
+      <ViewMotionWrapper className="mt-8" duration={1}>
         <PageHeading />
-      </div>
-      <Contact page />
-    </div>
-  )
-}
+      </ViewMotionWrapper>
 
-export default ContactPage
+      <ViewMotionWrapper delay={0.3} duration={1}>
+        <Contact page />
+      </ViewMotionWrapper>
+    </div>
+  );
+};
+
+export default ContactPage;
---
+++ app/(landing)/privacy-policy/page.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Head from "next/head";
+
+import styles from "./PrivacyPolicy.module.scss";
+
+const COLLECT = {
+  heading: "WHAT WE COLLECT",
+  description: "We may collect the following information:",
+  items: [
+    "Name and last name",
+    "contact information including email address and phone number",
+    "demographic information such as preferences and interests",
+  ],
+};
+
+const ACTION = {
+  heading: "WHAT WE DO WITH THE INFORMATION WE GATHER",
+  description:
+    "We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:",
+  items: [
+    "Internal record keeping.",
+    "To improve our products and services.",
+    "We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.",
+  ],
+};
+
+const PrivayPolicy = ({ contact }: { contact: any }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Poets Cxrner</title>
+      </Head>
+      <div className={styles.content}>
+        <div className={styles.heading}>
+          <h1>Privacy Policy</h1>
+          <p>
+            This privacy policy sets out how Four of Foes uses and protects any
+            information that you give to Four of Foes when you use this website.
+            Four of Foes is committed to ensuring that your privacy is
+            protected. Should we ask you to provide certain information by which
+            you can be identified when using this website, then you can be
+            assured that it will only be used in accordance with this privacy
+            statement. Four of Foes may change this policy from time to time by
+            updating this page. You should check this page from time to time to
+            ensure that you are happy with any changes.
+          </p>
+        </div>
+
+        <div className={styles.section}>
+          <h2>{COLLECT.heading}</h2>
+          <p>{COLLECT.description}</p>
+          <ul>
+            {COLLECT.items.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.section}>
+          <h2>{ACTION.heading}</h2>
+          <p>{ACTION.description}</p>
+          <ul>
+            {ACTION.items.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivayPolicy;
