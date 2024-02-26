d746b7373a3ed465c14b85b1621545d3875dba44
Reghardt Pienaar <reghardt7@gmail.com>
2024-02-26 17:38:49 +0200

    edge runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,8 @@ export const metadata: Metadata = {
   description: "The coolest tattoo website ever made",
 };
 
+export const runtime = 'edge'
+
 export default function RootLayout({
   children,
 }: {
