a9a56749e2f7b5cd892104e2ec6064cf8485cab1
Reghardt Pienaar <reghardt7@gmail.com>
2024-02-26 17:13:19 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
       </head>
       
       <body className={jost.className}>
