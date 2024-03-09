38e4b73fbf8e91865edd7ee18235c3b04edcc722
Reghardt Pienaar <reghardt7@gmail.com>
2024-03-09 10:59:08 +0200

    auth ts

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -5,14 +5,15 @@ import { fetchSanityClient } from "@/app/lib/actions/clients/fetchClient"
 import { redirect } from "next/navigation"
 
 import { ClientType, SessionType } from "@/types"
+import { NextRequest, NextResponse } from "next/server"
 
-export const protect = async (req: Request)=> {
+export const protect = async (req: NextRequest)=> {
   try{
-    const session = await getSession()
+    const session:any = await getSession()
       
-    if(!session) return redirect('/portal/auth/unauthorized')
-  
-    if(session?.expires > new Date(0))  return redirect('/portal/auth/unauthorized')
+    if (!session) {
+      return new NextResponse(null, { status: 401 });
+    }
   
     if(session){
       const client = await fetchSanityClient(session.user.id)
@@ -22,4 +23,24 @@ export const protect = async (req: Request)=> {
   } catch (error){
     console.log(error)
   }
+  // try {
+  //   const session:any = await getSession(req);
+    
+  //   if (!session) {
+  //     return new NextResponse(null, { status: 401 });
+  //   }
+  
+  //   const client = await fetchSanityClient(session.user.id)
+    
+  //   // Attach the client data to the request object
+  //   const clonedReq = req.clone();
+  //   clonedReq.headers.set("x-client", JSON.stringify(client));
+  
+  //   return NextResponse.next({
+  //     request: clonedReq,
+  //   });
+  // } catch (error) {
+  //   console.error("Error in authMiddleware:", error);
+  //   return new NextResponse(null, { status: 500 });
+  // }
 } 
