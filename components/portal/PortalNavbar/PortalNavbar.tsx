7ca17e0f31887e65abb3b541162869e0752e3845
Reghardt Pienaar <reghardt7@gmail.com>
2024-03-19 14:03:49 +0200

    smooth scroll

--- components/portal/PortalNavbar/PortalNavbar.tsx
+++ components/portal/PortalNavbar/PortalNavbar.tsx
@@ -1,82 +1,54 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 import { useSection } from "@/context/PortalSectionContext";
 
 const LINKS = [
-  {
-    link: "lobby",
-  },
-  {
-    link: "lounge",
-  },
-  {
-    link: "wallet",
-  },
+  { link: "lobby" },
+  { link: "lounge" },
+  { link: "wallet" },
 ];
 
 const PortalNavbar = () => {
   const { currentSection } = useSection();
   const [animateDot, setAnimateDot] = useState({});
+  const linkRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   useEffect(() => {
-    if (currentSection === "lobby") {
-      setAnimateDot({ x: "-28vw" });
-    }
-
-    if (currentSection === "lounge") {
-      setAnimateDot({ x: "0%" });
-    }
-
-    if (currentSection === "wallet") {
-      setAnimateDot({ x: "28vw" });
+    const activeLinkIndex = LINKS.findIndex((link) => link.link === currentSection);
+    if (activeLinkIndex !== -1) {
+      const activeLinkWrapper = linkRefs.current[activeLinkIndex];
+      const { left, width } = activeLinkWrapper!.getBoundingClientRect();
+      const containerLeft = activeLinkWrapper!.parentElement!.getBoundingClientRect().left;
+      const dotLeft = (left - containerLeft + width / 2) - 5;
+      setAnimateDot({ left: dotLeft });
     }
   }, [currentSection]);
 
-  const simulateArrowKeyPress = (arrowKey: any) => {
-    const event = new KeyboardEvent("keydown", { key: arrowKey });
-    document.dispatchEvent(event);
-  };
-
-  // const handleLinkClick = (link: string) => {
-  //   const main = document.getElementById('main');
-  //   console.log(main);
-  
-  //   const arrowLeftEvent = new KeyboardEvent('keydown', { key: 'ArrowLeft', bubbles: true });
-  //   main?.dispatchEvent(arrowLeftEvent);
-  
-  //   // Check if the event was dispatched successfully
-  //   console.log('Event dispatched:', arrowLeftEvent.defaultPrevented);
-
-  //   main!.addEventListener('keydown', (event) => {
-  //     // Handle the keydown event on the main element if needed
-  //     console.log('Key pressed on main element:', event.key);
-  //   });
-  // };
-  
-
-
   return (
-    <div className="grid grid-cols-3 gap-8 w-full">
+    <div className="grid grid-cols-3 gap-8 w-full h-full relative">
       {LINKS.map((link, i) => (
-        <a
-          className="uppercase text-[11px] tracking-[0.2em] flex flex-col items-center justify-center relative"
-          href={`#${link.link}`}
-          // onClick={() => handleLinkClick(link.link)}
-          key = {i}
+        <div
+          className="link-wrapper"
+          ref={(el) => (linkRefs.current[i] = el)}
+          key={i}
         >
-          {link.link}
-
-          {link.link === "lounge" && (
-            <motion.div
-              className="h-2 w-2 bg-[grey] rounded-full absolute bottom-[-15px]"
-              animate={animateDot}
-              transition={{ duration: 0.5 }}
-            />
-          )}
-        </a>
+          <a
+            className="uppercase text-[11px] tracking-[0.2em] flex flex-col items-center justify-center relative"
+            href={`#${link.link}`}
+          >
+            {link.link}
+          </a>
+          {/* {link.link === "lounge" && ( */}
+          <motion.div
+            className="h-2 w-2 bg-[grey] rounded-full absolute bottom-[-15px] mr-[1px]"
+            animate={animateDot}
+            transition={{ duration: 0.5 }}
+          />
+          {/* )} */}
+        </div>
       ))}
     </div>
   );
--- components/portal/sections/PostAppointment/PostAppointment.tsx
+++ components/portal/sections/PostAppointment/PostAppointment.tsx
@@ -24,12 +24,12 @@ const PostAppointment = ({ aftercareData, client }: { aftercareData: AftercareTy
       <div className="h-[73%] w-full relative">
         <Image src={"/portal-bg.png"} fill alt="ted" className="object-cover" />
       </div>
-      <header className="text-center flex flex-col items-center gap-4 h-[8%]">
+      <header className="text-center flex flex-col items-center gap-2 h-[8%] mt-2">
         <h1 className="tracking-[0.6em] text-[8px] uppercase">Thank you for your patronage</h1>
         <Asterisk size={8} />
       </header>
       <div className="min-w-[45%] w-[60%] h-[17%] mx-auto md:min-w-[20%] flex items-center relative">
-        <div className="absolute left-0 bottom-[20px] lg:bottom-0 w-full ">
+        <div className="absolute left-0 bottom-[40px] lg:bottom-[20px] w-full ">
           <PortalSectionAccordion
             Section1 = {<AftercareAccordion data = {aftercareData} />} 
             Section2 = {<TattooImagesAccordion images={client.tattooImages} />}
