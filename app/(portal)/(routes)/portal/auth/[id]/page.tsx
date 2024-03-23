4161c518ae49d93f9c29585e20b68c85f702a97d
Reghardt Pienaar <reghardt7@gmail.com>
2024-03-23 11:05:35 +0200

    .

--- app/(portal)/(routes)/portal/auth/[id]/page.tsx
+++ app/(portal)/(routes)/portal/auth/[id]/page.tsx
@@ -4,10 +4,14 @@ import React from 'react'
 import { fetchSanityClient } from '@/app/lib/actions/clients/fetchClient'
 
 import PortalAuth from '@/components/portal/sections/PortalAuth'
+import { getSession } from '@/app/lib/actions/clients/auth'
 
 const PortalAuthPage: React.FC<any> = async ({ params }) => {
   const { id } = params
+  const session = await getSession()
 
+  if(session) redirect('/portal/pre')
+  
   const client = await fetchSanityClient(id)
 
   if(!client) return redirect('/')
