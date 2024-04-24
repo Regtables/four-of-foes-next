7e9652d9890e07a9de3bcb60e4a4eebfee442ec2
Reghardt Pienaar <reghardt7@gmail.com>
2024-04-24 10:57:47 +0200

    testing

--- app/(portal)/(routes)/portal/layout.tsx
+++ app/(portal)/(routes)/portal/layout.tsx
@@ -7,7 +7,7 @@ export const revalidate = 0
 const PortalRootLayout = async ({ children } : { children: React.ReactNode }) => {
   return (
     <PortalProgressProvider>
-      <div className="min-h-[100vh] w-[100vw] h-screen max-h-screen overflow-hidden flex flex-col">
+      <div className="min-h-[96vh] lg:pb-0 mb-[4vh] w-[100vw] h-[90vh] lg:h-screen lg:min-h-screen max-h-screen overflow-hidden flex flex-col">
         <header className="flex justify-center items-center flex-[0.05] z-10 relative w-[160px] mx-auto">
           <h1 className="uppercase tracking-wider font-[300] heading-font">Four of Foes</h1>
         </header>
