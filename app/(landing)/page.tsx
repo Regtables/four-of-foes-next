c980e213decc4006d3d9865ad89bc6cb7029c8a7
Reghardt Pienaar <reghardt7@gmail.com>
2025-06-27 11:33:30 +0200

    testing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,6 +8,8 @@ export const revalidate = 60
 export default async function Home() {
   const bookingFormData = await fetchBookingFormContent();
 
+  console.log("Booking Form Data:", bookingFormData);
+
   return (
     <div className="max-h-100vh overflow-hidden">
       <Suspense>
